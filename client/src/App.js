cef9efff53563948f68c5ca456fb50139647d4f1
Arvinder Singh <sokhipc@gmail.com>
2025-06-26 01:55:07 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import io from "socket.io-client";
 import "./App.css";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://gossip-io-lyst.onrender.com");
 
 function App() {
   const [msg, setMsg] = useState("");
